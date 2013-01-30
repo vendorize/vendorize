6475e1acb7338cdddeb840b5daead0e2b07897f8
Bram Swenson <bram@craniumisajar.com>
2013-01-29 22:15:25 -0800

    [CONFIG] generator skel in place with empty config

--- lib/generators/vendorize/config_generator.rb
+++ lib/generators/vendorize/config_generator.rb
@@ -1,7 +1,8 @@
 module Vendorize
   class ConfigGenerator < Rails::Generators::Base
     def generate_vendorize_config
-      say 'creating config'
+      create_file 'config/vendorize.rb', <<-EOF
+      EOF
     end
   end
 end
